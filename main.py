8a439c008d458464d221f3f03ba7b424e4a0a2ea
Weboscopy <weboscopy@gmail.com>
2025-01-04 08:55:52 +0100

    tuto 03 sur les fonctions

--- main.py
+++ main.py
@@ -1,174 +1,193 @@
-# list 
-users = ["Tom", "Zoé", "Sara", "Chloé"]
-mylist = list([1, "tom@mail.com", True])
-empty_list = []
-
-print(users[0])
-print(users[-1])
-print(users[0:2])
-print(users[:2])
-print(users[:-1])
-print(users[::2])
-print(users[::-1])
-print(users.index("Sara"))
-print("Zoé" in users)
-
-users.append("Elsa")
-print(users)
-users.extend(["Mia", "Nora"])
-print(users)
-users += ["Adam", "Jade"]
-print(users)
-users.insert(1, "Yanis")
-print(users)
-
-users[2:2] = ["Alex", "Julie"] # insetion 
-print(users)
-users[1:3] = ["Anna", "Nora"] # remplacer
-print(users)
-
-users.pop()
-print(users)
-users.remove("Anna")
-print(users)
-del users[0]
-print(users)
-
-mylist.clear()
-print(mylist)
-
-users.sort()
-print(users)
-users[0] = "adam"
-users.sort(key=str.lower)
-print(users)
-users.reverse()
-print(users)
-
-print(sorted(users)) # crée une copie de la liste users
-
-for item in users:
-    print(item)
-
-for index, item in enumerate(users):
-    print(index, item)
-
-users_string = ','.join(users)
-print(users_string)
+def hello():
+    print("hello")
 
 
-# tuple
-black = (255, 255, 255)
-# black[0] = 32
-(red, *rest) = black 
-print(red, rest)
-(red, *green, blue) = black 
-print(red, green, blue)
-white = tuple((0,0,0))
-print(black.count(255))
-print(white*3)
+hello()
+hello()
+hello()
+hello()
+hello()
+hello()
 
-names = ["Alice", "Zoé", "Yanis"]
-scores = [20, 30, 12]
-results = list(zip(names, scores))
-print(results)
-for name, score in results:
-    print(name, score)
+# def add(num1, num2):
+#     print(num1 + num2)
 
-color = {"red" : 120, "green": 20, "blue" : 139}
-color["red"] = 222
-print(color)
-from collections import namedtuple
-Color = namedtuple("Color", ["red", "green", "blue"])
-color = Color(66, 120, 88)
-color = Color(red=66, green=120, blue=88)
-# color[0] = 20
-print(color[0])
-print(color.red)
+# add(3, 7)
+
+def add(num1, num2):
+   return num1 + num2
 
 
-# set 
-myset = {1,2,3,4,4,4}
-print(myset)
-myset2 = set((1,2,3,4,4))
-empty_set = set()
+result = add(8, 9)
 
-nums_list = [2,4,1,8,8,7,7,1,1,1,1]
-nums_set = set(nums_list)
-print(nums_set)
+print(result)
 
-original_list = [1,2,3,4,4,4,8]
-unique_list = list(set(original_list))
-print(unique_list)
+# paramètres par défaut
 
-print(min(nums_set))
-print(max(nums_set))
-print(sum(nums_set))
-print(sum(nums_list))
-print(3 in nums_set)
+def add_default(num1, num2=2):
+    return num1 + num2 
 
-nums_set.remove(2)
-# nums_set.remove(3)
-nums_set.discard(3)
 
-nums_set.add(9)
-others = {3,5}
-nums_set.update(others)
-print(nums_set)
-
-a = {1,2,3}
-b = {4,5,6}
-c = a.union(b)
-c = a | b 
+c = add_default(2)
 print(c)
 
-a = {1,2,3}
-b = {2,3,4}
-c = a & b 
-print(a.intersection(b))
+# fonctions variadiques
 
-print(a.difference(b))
-c = a - b
+def add_multiple(*args):
+    mysem = 0 
+    for i in args:
+        mysem += i 
+    print(mysem)
 
-print(a.symmetric_difference(b))
-c = a ^ b 
 
-a.symmetric_difference_update(b)
-print(a)
+add_multiple(1,2,3,4,5,6,7)
 
-# dictionnaire
-post = {"id" : 1, "author": "Tom", "published" : True}
-product = dict(name="T-shirt", price=20)
-empty_dict = {}
-print("title" in post)
 
-keys = list(post.keys())
-values = list(post.values())
-items = list(post.items())
-print(keys)
-print(values)
-print(items)
+def say_hello(**kwargs):
+    print(f"Salut {kwargs["first"]} {kwargs["last"]}")
 
-for key, val in post.items():
-    print(f"{key} : {val}")
+say_hello(first="John", last="Lenon")
 
-print(post.get("published"))
-print(post["author"])
 
-post["author"] = "Yanis"
-print(post)
-post.update({"author" : "Zoé", "published" : False})
-print(post)
+# composition de fonctions
+print(round(abs(float(input(" Entrez un number : ")))))
 
-post.popitem()
-print(post)
-del post["author"]
-print(post)
+# fonctions callback
 
-students = {"Tom": 9, "Zoé": 7, "Alice": 7, "Yanis": 9, "Nora": 8, "Sara": 8, "Matteo": 7}
-scores = list(students.values())
+def process_nums(numbers, callback):
+    processed = []
+    for number in numbers:
+        processed.append(callback(number))
+    return processed
 
-for score in set(scores):
-    num = scores.count(score)
-    print(f"{num} élèves ont obtenu {score}")
+def square(x):
+    return x * x 
 
+def double(x):
+    return 2 * x
+
+print(process_nums([1,2,3,4,5], double))
+
+# la portée des variables
+
+name = "Tom"
+
+def get_name():
+    name = "John"
+    print(name)
+
+get_name()
+print(name)
+
+def get_name():
+    global name 
+    name = "John"
+    print(name)
+
+get_name()
+print(name)
+
+
+def get_score():
+    score = 20 
+    def update_score():
+        score = 30
+    
+    print(score)
+    update_score()
+    print(score)
+
+get_score()
+
+def get_score():
+    score = 20 
+    def update_score():
+        nonlocal score 
+        score = 30
+    
+    print(score)
+    update_score()
+    print(score)
+
+get_score()
+
+# closures
+
+def counter(val):
+    print(f"Montant initial {val}")
+    def decrement():
+        nonlocal val 
+        val -= 10 
+        if(val <= 0):
+            print("Votre compteur est vide")
+        else:
+            print(f"Il vous rest {val} dans votre compteur")
+
+    return decrement
+
+charge = counter(40)
+
+charge()
+charge()
+charge()
+charge()
+charge()
+
+# fonctions curry 
+def multiply_standard(num1, num2):
+    return num1 * num2 
+
+print(multiply_standard(2,3))
+
+def multiply_curry(num1):
+    def by(num2):
+        return num1 * num2 
+    
+    return by 
+
+
+print(multiply_curry(2)(3))
+
+# fonctions partielles 
+multiply_by_10 =  multiply_curry(10)
+
+print(multiply_by_10(2))
+print(multiply_by_10(3))
+print(multiply_by_10(4))
+print(multiply_by_10(5))
+print(multiply_by_10(6))
+
+# fonctions lambda 
+def multiply_lambda(num1):
+    return lambda num2 : num1 * num2 
+
+multiply_by_3 = multiply_lambda(3)
+print(multiply_by_3(4))
+print(multiply_by_3(5))
+
+# fonctions récursives
+
+user = {
+    'username': "tom",
+    'location': {
+        'city': "Rome",
+        'coord': {
+            'lat': 44.2,
+            'lng': 32.1
+        }
+    },
+    'age': 28
+}
+
+def get_all_values(d):
+    values = []
+
+    for value in d.values():
+        if not isinstance(value, dict):
+            values.append(value)
+        else:
+           values.extend(get_all_values(value))
+
+    return values
+
+print(get_all_values(user))
