3b60b5368cc515e1e3ff27ebd2047ba7fce433f0
Weboscopy <weboscopy@gmail.com>
2025-01-25 13:43:11 +0100

    tuto 07 - système de fichiers

--- main.py
+++ main.py
@@ -1,144 +1,258 @@
-# date 
-from datetime import date 
-d = date(2016, 7, 23)
-print(d)
-d = date.fromisoformat("2015-07-23")
-print(d)
-
-today = date.today()
-print(today)
-
-print(today.day)
-print(today.month)
-print(today.year)
-print(today.weekday()) # indexé de 0 à 6
-print(today.isoweekday())  # indexé de 1 à 7
-
-one_year_from_today = today.replace(year = today.year + 1)
-print(one_year_from_today)
-
-# formatting
-import locale 
-locale.setlocale(locale.LC_TIME, "fr_FR")
-print(f"Le {today: %d %B %Y} tombe un {today:%A} qui est le {today:%j} jour de l'année")
-print(today.isoformat(), type(today.isoformat()))
-
-# time 
-from datetime import time as datetime_time
-t = datetime_time(9, 30, 45)
-print(t)
-print(t.minute)
-print(t.hour)
-
-# datetime 
-from datetime import datetime
-dt = datetime(2024, 12, 22, 10, 24, 44)
-print(dt)
-print(dt.year)
-print(dt.minute)
-print(dt.date())
-print(dt.time())
-
-now = datetime.now()
-print(now)
-
-
-# files' date 
+# os 
 import os 
-print(os.stat("demo.txt"))
-modified_time = os.stat("demo.txt").st_mtime # temps unix, temps en sec depuis 01 janv 1970 
-print(datetime.fromtimestamp(modified_time))
 
-# date from string 
-date_string = "12 juillet 1997"
-date_obj = datetime.strptime(date_string, "%d %B %Y")
-print(date_obj)
+# navigation 
+print(os.getcwd())
 
-# date to string 
-today_string = today.strftime("Le %A %d %b %Y")
-print(today_string)
+os.chdir("dossier1")
 
-# time delta 
-from datetime import timedelta 
-tdelta = timedelta(hours=12)
-print(tdelta)
-tdelta = timedelta(days=7)
-print(tdelta)
-print(today + tdelta)
+print(os.getcwd())
 
-bthday = date.fromisoformat("2025-08-21")
-till_birthday = abs(bthday - today)
-print(f"Il reste {till_birthday} jours avant mon anniversaire")
-print(f"Il reste {till_birthday.total_seconds()} secondes avant mon anniversaire")
+# chemins absolus 
 
-# timezone 
-from datetime import timezone 
-now_utc = datetime.now(tz=timezone.utc)
-print(now_utc)
+os.chdir("C:\\Users\\Louis\\Desktop\\python\\07 - système de fichiers\\dossier2")
 
-france_offset = timedelta(hours=2)
-now_france = now_utc.astimezone(timezone(france_offset))
-print(now_france)
+print(os.getcwd())
 
-import zoneinfo 
-available_timezones = zoneinfo.available_timezones()
-print(available_timezones)
+os.chdir(r"C:\Users\Louis\Desktop\python\07 - système de fichiers")
 
-now_utc = datetime.now(tz=zoneinfo.ZoneInfo("UTC"))
-print(now_utc)
+print(os.getcwd())
 
-now_france = now_utc.astimezone(zoneinfo.ZoneInfo("Europe/Paris"))
-print(now_france)
+# conditions
 
-# time 
-import time 
-print("avant")
-time.sleep(1)
-print("après")
-print(time.ctime(0))
-print(time.time())
-print(time.ctime(time.time()))
-print(time.ctime())
+filepath = r"dossier2\demo.txt"
 
-time_struct_utc = time.gmtime()
-print(time_struct_utc)
-time_struct_local = time.localtime()
-print(time_struct_local)
-time_tuple = (2020, 4, 20, 20, 30, 0, 0, 111, 1)
-time_string = time.asctime(time_tuple)
-print(time_string)
-unix_time = time.mktime(time_tuple)
-print(unix_time)
-unix_today = time.mktime(today.timetuple())
-print(unix_today)
+if os.path.exists(filepath):
+    print("Ce chemin existe")
+    if os.path.isfile(filepath):
+        print("Il s'agit d'un fichier")
 
-# calendar 
+folder_path = "dossier2"
 
-import calendar 
+if os.path.isdir(folder_path):
+    print("Il s'agit d'un dossier")
 
-year = 2024 
+# combiner des chemins 
 
-for month in range(1, 13):
-    print(calendar.month_name[month])
-    print(calendar.month(year, month))
-    print(calendar.monthrange(year, month))
-    print(calendar.monthrange(year, month)[0])
-    print(calendar.monthrange(year, month)[1])
+input_folder = os.path.join(os.getcwd(), r"input_dir\data")
 
-# cas concret 
-balance = 4000 
-monthly_payment = 300 
+# création
+if not os.path.exists(input_folder):
+    os.makedirs(input_folder)
 
-today = date.today()
+# déplacement 
+src = r"dossier2\demo.txt"
+dest = "demo.txt"
 
-days_in_current_month = calendar.monthrange(today.year, today.month)[1]
+try:
+    os.replace(src, dest)
+except FileNotFoundError:
+    print("Ce chemin n'existe pas")
 
-days_until_end_month = days_in_current_month - today.day 
+# renommage 
+try:
+    os.rename("demo.txt", "renamed_demo.txt")
+except FileNotFoundError:
+    print("Ce chemin n'existe pas")
 
-end_date = today + timedelta(days=days_until_end_month + 1)
+# suppression 
+try:
+    os.rmdir("dossier3")
+except FileNotFoundError:
+    print("Ce chemin n'existe pas")
 
-while balance > 0 :
-    balance -= monthly_payment
-    print(end_date, balance)
-    days_in_current_month = calendar.monthrange(end_date.year, end_date.month)[1]
-    end_date = end_date + timedelta(days= days_in_current_month)
+try:
+    os.removedirs(r"dossier4\2024\janvier")
+except FileNotFoundError:
+    print("Ce chemin n'existe pas")
+
+# variables d'environnement 
+os.environ["HOME"] = r"user\home"
+
+home_dir = os.path.join(os.getcwd(), os.environ.get("HOME"))
+
+print(home_dir)
+
+# informations sur les fichiers
+print(os.stat("renamed_demo.txt"))
+
+# méthodes utiles 
+filepath = os.path.join(os.getcwd(), "renamed_demo.txt")
+print(os.path.basename(filepath))
+print(os.path.dirname(filepath))
+print(os.path.split(filepath))
+print(os.path.splitext(filepath))
+print(os.path.abspath(__file__))
+
+# lancer un fichier 
+# os.startfile(filepath)
+
+# boucles
+for filepath in os.listdir():
+    print(filepath)
+
+os.chdir("courses")
+
+# for f in os.listdir():
+#     f_name, f_ext = os.path.splitext(f)
+#     f_title, f_course, f_num = f_name.split("-")
+#     new_name = f"{f_num}-{f_title}{f_ext}"
+#     os.rename(f, new_name)
+
+os.chdir(os.pardir)
+
+for dirpath, dirnames, filenames in os.walk(os.getcwd()):
+    print(dirpath)
+    if dirnames:
+        print(dirnames)
+    if filenames:
+        print(filenames)
+
+
+src = os.path.join(os.getcwd(), "src")
+images = []
+image_extensions = ['*.jpg', '*.jpeg', '*.png', '*.gif', '*.bmp', '*.tiff']
+
+from fnmatch import fnmatch
+
+for root, dirs, files in os.walk(src):
+    for filename in files:
+        for ext in image_extensions:
+            if fnmatch(filename, ext):
+                images.append(filename)
+
+print(images)
+
+# shutil 
+import shutil 
+
+os.chmod("log.txt", 0o444) # lecture seule 
+
+# copies
+try:
+    shutil.copyfile("log.txt", "copies\\copy1.txt")
+    shutil.copy("log.txt", "copies\\copy2.txt")
+    shutil.copy2("log.txt", "copies\\copy3.txt")
+except FileNotFoundError:
+    print("ce chemin n'existe pas")
+except PermissionError:
+    print("Vous n'avez pas la permission")
+
+
+print(os.stat(r"copies\copy1.txt").st_mode)
+print(os.stat(r"copies\copy2.txt").st_mode)
+
+print(os.stat("log.txt").st_mtime)
+print(os.stat(r"copies\copy1.txt").st_mtime)
+print(os.stat(r"copies\copy2.txt").st_mtime)
+print(os.stat(r"copies\copy3.txt").st_mtime)
+
+try:
+    shutil.rmtree("folder")
+except FileNotFoundError:
+    print("ce chemin n'existe pas")
+
+# glob
+import glob 
+
+jpg_paths = glob.glob("src/*.jpg")
+print(jpg_paths)
+
+jpg_paths = glob.glob("src/**/*.jpg", recursive=True)
+print(jpg_paths)
+
+# Pathlb 
+from pathlib import Path 
+
+print(Path.cwd())
+
+filepath = Path("dossier1")
+filepath = Path(r"C:\Users\Louis\Desktop\python\07 - système de fichiers")
+filepath = Path.cwd() / "dossier1"
+print(filepath)
+filepath = Path(__file__).parent
+print(filepath)
+
+# conditions
+if filepath.exists():
+    print("ce chemin existe")
+
+# boucles
+for p in Path("courses").iterdir():
+    print(p)
+
+root_path = Path("src")
+
+for path in root_path.glob("*"):
+    print(path)
+
+for path in root_path.rglob("*"):
+    print(path)
+
+for path in root_path.rglob("*.jpg"):
+    print(path)
+
+for path in root_path.rglob("*.jpeg", case_sensitive=False):
+    print(path)
+
+# match
+logfile = Path("log.txt")
+print(logfile.match("*.txt"))
+
+# suppression 
+filepath = Path(__file__).parent / "dossier1" / "demo.txt"
+try:
+    filepath.unlink()
+except FileNotFoundError:
+    print("ce chemin n'existe pas")
+
+# création  de fichiers
+filepath.touch()
+
+# création de dossiers
+Path("parent_dir/output_dir").mkdir(parents=True, exist_ok=True)
+
+# écrire et lire des fichiers
+filepath.write_text("hello")
+print(filepath.read_text())
+
+# renommage
+p = Path("renamed_demo.txt")
+p.rename("demo.txt")
+
+# déplacement 
+Path("demo.txt").replace("dossier2/demo.txt")
+
+# informations sur les chemins
+print(Path().cwd().stat().st_mtime)
+
+# propriétés utiles
+print(filepath.name)
+print(filepath.stem)
+print(filepath.suffix)
+print(filepath.parent)
+
+# évaluation 
+print(Path(__file__) == Path.cwd() / "main.py")
+
+# méthodes utiles
+print(Path("dossier1"))
+print(Path("dossier1").absolute())
+print(Path("dossier1\..").absolute())
+print(Path("dossier1\..").resolve())
+print(Path("dossier1\..").resolve().as_posix())
+
+# boucles
+src = Path.cwd() / "src"
+
+images = []
+
+image_extensions = ['*.jpg', '*.jpeg', '*.png', '*.gif']
+
+for file_path in src.rglob("*"):
+    if file_path.is_file(): 
+        for ext in image_extensions:
+            if file_path.match(ext):
+                images.append(file_path.name)
+
+print(images)
